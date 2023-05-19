548b091a11b79b290efc0695b47d13475b325a02
Helin Akdoğan <akdoganhelin@gmail.com>
2023-05-19 23:26:27 +0300

    19.05.2023

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 
+// 1. mapping for array
+// 2. conditional rendering
+
+
 const ListGroup = () => {
+
+  let items = ["New York", "Tokyo", "London"];
+  items = [];
+ 
+  // 2
+  const getMessage = () => {
+    items.length === 0 ? <p>no item found</p> : null;
+  }
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+
+      {/* 2 */}
+      {getMessage}
+
+      <ul className="list-group">
+        {/* 1 */}
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 };
 
---
+++ react-app/src/components/ListGroup2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ListGroup2 = () => {
+
+    let items = ["New York", "Tokyo", "London"];
+    
+  return (
+    <div>ListGroup2</div>
+  )
+}
+
+export default ListGroup2
